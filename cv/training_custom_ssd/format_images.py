7962e7a28f6459b1ea76186a4022e22f1891ec7d
chalkfung <chalkfung@gmail.com>
2021-08-22 21:53:28 +0800

    Added labels xml

--- cv/training_custom_ssd/format_images.py
+++ cv/training_custom_ssd/format_images.py
@@ -31,7 +31,22 @@ def assignRandomNames(baseDir):
             os.rename(filepath, finalFolder + os.sep + id_generator()+file_extension)
 
 
+def createTrainvalTxt(baseDirDataSet):
+    buffer = ''
+    baseDir = baseDirDataSet+'\images'
+    for filename in os.listdir(baseDir):
+        filenameOnly, file_extension = os.path.splitext(filename)
+        # print (file_extension)
+        s = 'images/'+filenameOnly+'.jpg'+' '+'labels/'+filenameOnly+'.xml\n'
+        print (repr(s))
+        img_file, anno = s.strip("\n").split(" ")
+        print(repr(img_file), repr(anno))
+        buffer+=s
+    with open(baseDirDataSet+'\\structure\\trainval.txt', 'w') as file:
+        file.write(buffer)  
+    print('Done')   
+
 # Usage
 baseDir = "/home/pi/Github/OIP-2021-Team1/cv/training_custom_ssd/data"
-assignRandomNames(baseDir)
+createTrainvalTxt(baseDir)
 # baseDir = 'dir'
