329dd4f9f9eb8ac673570613c063fa3e6d6dcd4e
Cheuk Fung Ng <chalkfung@gmail.com>
2021-08-22 19:25:55 +0800

    formatted images

--- cv/training_custom_ssd/format_images.py
+++ cv/training_custom_ssd/format_images.py
@@ -2,3 +2,23 @@ from PIL import Image
 import os
 
 from os.path import basename
+
+def resizeImages(baseDir):
+    basewidth = 558
+    for filename in os.listdir(baseDir):
+        filenameOnly, file_extension = os.path.splitext(filename)
+        # print (file_extension)
+        if (file_extension in [".jpeg", '.jpg']):
+            filepath = baseDir + os.sep + filename
+            img = Image.open(filepath)
+            wpercent = (basewidth/float(img.size[0]))
+            hsize = int((float(img.size[1])*float(wpercent)))
+            img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+            img.save(filepath)
+            print (filenameOnly, "Done")
+    print('Done')
+         
+# Usage
+baseDir = "/home/pi/Github/OIP-2021-Team1/cv/training_custom_ssd/data"
+resizeImages(baseDir)
+# baseDir = 'dir'
