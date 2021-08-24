140d35e0bf7b1b4c0f93f46ecfa7cc50aea7cc28
catkane-doodles <tanpeikai@hotmail.com>
2021-08-24 20:15:00 +0800

    Add arduino code

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 cd server
 export FLASK_APP=app
 flash run &
+export DISPLAY=:0
 chromium-browser --kiosk --fullscreen http://localhost:5000/index.html  
--- serial/test.py
+++ serial/test.py
@@ -2,9 +2,11 @@ import serial
 import time
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-    ser.flush()
-
     while True:
-        ser.write(b"ABCD")
+        ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+        ser.write(1)
+        ser.flush()
+        # print("running")
         time.sleep(1)
+        ser.close()
+
